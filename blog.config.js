220d92d8f960230bfa76760f157b6b3d632275c8
Timking13999 <dawang13999@gmail.com>
2025-02-26 18:07:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'King', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '芳疗领航员', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '芳香疗法,精油,芳疗,芳疗师,IFA芳疗,芳疗证书', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '芳香疗法,精油,芳疗,芳疗师,IFA芳疗,芳疗证书,国际芳疗师，NAHA芳疗，芳疗学习,芳疗培训机构,法系芳疗,科学芳疗,医学芳疗,临床芳疗,学芳疗', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
